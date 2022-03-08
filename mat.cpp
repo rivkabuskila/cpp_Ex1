ce8bc0007502ab0cb4785a1b368bf8d13db3568f
rivkabuskila <93676748+rivkabuskila@users.noreply.github.com>
2022-03-08 22:16:13 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,15 @@
+//
+// Created by rivka on 06/03/2022.
+#include "mat.hpp"
+#include <iostream>
+
+using namespace std;
+
+//
+
+namespace ariel {
+    string mat(int column, int row, char symbol1, char symbol2) {
+
+        return "";
+    }
+}
